6d9662a0669f7137994d0724656ad821bdb4a562
LuigiTroiano <troiano.luigi8@gmail.com>
2020-12-03 17:49:59 +0100

    Punto 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,31 @@ var studente = {
   age: "24",
 }
 // console.log(studente);
-for (const key in studente) {
+for (var key in studente) {
   console.log(key,studente[key]);
+  document.getElementById("paragrafo").innerHTML += " " + studente[key];
 }
 // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+StudentesseCheAvreiVolutoAvereInClasse  = [
+  {
+    name: "Emily",
+    surname: "Ratajkowski",
+    age: 29,
+  },
+  {
+    name: "Miriam",
+    surname: "Leone",
+    age: 35,
+  },
+  {
+    name: "Anne",
+    surname: "Hathaway",
+    age: 38,
+  }
+];
+//console.log(StudentesseCheAvreiVolutoAvereInClasse);
+for (var i = 0; i < StudentesseCheAvreiVolutoAvereInClasse.length; i++) {
+  console.log("Studente: " + StudentesseCheAvreiVolutoAvereInClasse[i].name, StudentesseCheAvreiVolutoAvereInClasse[i].surname);
+  }
+
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
