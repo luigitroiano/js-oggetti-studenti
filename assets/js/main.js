aba8cb1fbd39c72f87923fe79e0b5cfc25f3831f
LuigiTroiano <troiano.luigi8@gmail.com>
2020-12-03 16:52:04 +0100

    Punto 1

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+var studente = {
+  name: "Luigi",
+  surname: "Troiano",
+  age: "24",
+}
+// console.log(studente);
+for (const key in studente) {
+  console.log(key,studente[key]);
+}
+// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
